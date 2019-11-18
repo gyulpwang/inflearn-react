8d3f644cb71d31f48ba6c2a0407cdc26175b78f5
gyulpwang <56156723+gyulpwang@users.noreply.github.com>
2019-11-18 21:03:52 +0900

    LifeCycle
    LifeCycle API 소개 및 사용법

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 import App from "./App.js";
+import App2 from "./App2.js";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+//ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App2 />, rootElement);
