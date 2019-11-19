3ec366988a907116e6fda20b89163c5e676546dc
gyulpwang <rbfla927@gamil.com>
2019-11-19 11:10:25 +0900

    Input 상태 관리하기

---
+++ contact-app/src/components/PhoneForm.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+/*
+꿀팁!
+왼쪽 사이드바 가장 하단의 테트리스 모양 아이콘(확장 프로그램)을 클릭하여 reactjs code snippet이라고 검색하면
+charalampos karypidis라는 사람이 만든 Reactjs code snippets라는 확장 프로그램이 있는데,
+이걸 설치 후 rcc라고 하면 클래스형 컴포넌트, rcs라고 하면 함수형 컴포넌트를 자동으로 생성해준다!
+*/
+class PhoneForm extends Component {
+
+    state = {
+        name : '',
+        phone : '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            // 객체 안에서 사용되는 [e.target.name] : "속성 계산명"이라는 문법
+            // e.target = input
+            // e.target.value = input의 value 값
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render(){
+        return(
+            <form>
+                <input 
+                    name="name"
+                    placeholder="이름" 
+                    onChange={this.handleChange} 
+                    value={this.state.name} 
+                />
+                <input
+                    name="phone"
+                    placeholder="전화번호"
+                    onChange={this.handleChange}
+                    value={this.state.phone}
+                />
+                <div>
+                    {this.state.name} {this.state.phone}
+                </div>
+            </form>
+        )
+    }
+}
+
+export default PhoneForm;
